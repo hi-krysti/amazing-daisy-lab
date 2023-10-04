61b659dac184248470d470b03b720bfa327d26a6
Krysti <hi.krysti@gmail.com>
2023-10-04 19:43:13 -0400

    pizza

---
+++ exercises.py
@@ -0,0 +1,53 @@
+# print('hello')
+# print('world')
+
+word_one = 'abcdefg'
+# print(word)
+
+# print(len(word))
+
+# Start
+# result = word[0]
+# print(result)
+
+# Start from another index
+# return c
+# result = word[2]
+# print(result)
+
+# Range from start to stop
+# result = word[0:2]
+# print(result)
+
+# Start, Stop, and Step
+# result = word[2:8:2]
+# print(result)
+
+# Starts from the end
+# result = word[-2]
+# print(result)
+
+# Splicing
+word = 'Greetings'
+print(word)
+print(word[0])
+print(word[0:3])
+print(word[2:4])
+print(len(word))
+print(word[0:len(word)])
+print(word[-1])
+print(word[-4:-2])
+
+# Concatenation
+print(word + word_one)
+
+#Arithmatic
+print(1 + 2)
+print(1.5 + 2.3)
+print(word * 3)
+
+print(word.find('t'))
+print(word.find('q'))
+print(word.find('e'))
+
+x = pizza
---
+++ main.py
@@ -0,0 +1,35 @@
+first_name = "Krysti"
+
+my_first_var = 3
+
+hi123 = 123
+# print(hi123)
+# print(first_name)
+
+# ordering = input("What would you like? ")
+# print(ordering)
+
+# y = my_first_var
+y = first_name
+
+# Adding variables
+# x = y + 3
+# x = y + "3"
+# print(x)
+
+# Subtracting variables
+# x = y - 7
+# print(x)
+
+# Multiplying Variables
+# total = y * 5
+# print(total)
+
+# Modulus
+extra = 13 % 5
+print(extra)
+
+title = "Ms. " + first_name
+print(title)
+
+I love pizza
